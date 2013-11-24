dfeaab26ac18d969b2389faa053ec0e4f5d11ffa
Phillip Sorensen <phil.a.sorensen@gmail.com>
2013-11-23 19:34:21 -0500

    New init.el and custom.el files

---
+++ init.el
@@ -0,0 +1,69 @@
+;;; init.el --- base init file for Emacs
+
+;; This file is in the .emacs.d directory where it is under git.  You
+;; need make sure that there is either no ~/.emacs file or the this file
+;; is linked to ~/.emacs.
+;;
+;; The current initialization is for Emacs 24.3.
+
+;;;; Throw error if not right version 
+
+(unless (>= emacs-major-version 24)
+  (unless (or (> emacs-major-version 24) (>= emacs-minor-version 3))
+    (setq inhibit-startup-screen 1)
+    (error "The init.el file only works for version 24.3 or later")))
+
+
+
+;;;; basic setup 
+
+;; Turn off interface elements
+(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; no startup screen
+(setq inhibit-startup-screen 1)
+
+;; Customizations in separate files
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
+
+;; initialize packages.el
+(require 'package)
+
+(defun require-package (package &optional min-version no-refresh)
+  "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not be
+re-downloaded in order to locate PACKAGE."
+  (unless (package-installed-p package min-version)
+    (if (or (assoc package package-archive-contents) no-refresh)
+        (package-install package)
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
+
+(add-to-list 'package-archives 
+	     '("melpa" . "http://melpa.milkbox.net/packages/"))
+
+(package-initialize)
+
+;; Define the after macro
+(defmacro after (feature &rest body)
+  "After FEATURE is loaded, evaluate BODY."
+  (declare (indent defun))
+  `(eval-after-load ,feature
+     '(progn ,@body)))
+
+
+
+;;;; load modular init pieces
+
+;; Add ~/.emacs.d/init to search path
+(add-to-list 'load-path (expand-file-name "init" user-emacs-directory))
+
+
+
+;;;; Local Customizations (not stored in version control)
+
+(load (expand-file-name "local.el" user-emacs-directory))
