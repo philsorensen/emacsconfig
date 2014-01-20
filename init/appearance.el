2d5e419dfb69b601501028ccae263f98225a54ec
Phillip Sorensen <phil.a.sorensen@gmail.com>
2014-01-20 15:30:55 -0500

    Add line-numbers to graphical frames

--- init/appearance.el
+++ init/appearance.el
@@ -6,11 +6,19 @@
 
 ;; Set frame parameters for non-console windows
 (when window-system
+  ; enable menu
   (menu-bar-mode 1)
 
+  ; turn on linum mode
+  (setq linum-format "%4d")
+  (global-linum-mode 1)
+
+  ; set font to system default
   (setq font-use-system-font t)
 
-  (add-to-list 'default-frame-alist '(width . 80))
+  ; set frame size
+  (add-to-list 'default-frame-alist '(width . 85))
   (add-to-list 'default-frame-alist '(height . 40)))
+
 
 (provide 'appearance)
