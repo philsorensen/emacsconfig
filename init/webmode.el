2e0f2be47e258100bb9a6145e18c9875712f5a5a
Phillip Sorensen <phil.a.sorensen@gmail.com>
2015-12-29 22:25:47 -0500

    Add .erb to webmode list and tweak webmode settings

--- init/webmode.el
+++ init/webmode.el
@@ -8,6 +8,7 @@
 
 
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (setq web-mode-engines-alist
       '(("django" . "\\.html\\'")))
 
@@ -37,7 +38,9 @@
             (auto-fill-mode 1)
             (flyspell-mode 1)
             (emmet-mode 1)
-            (setq web-mode-markup-indent-offset 2)))
+            (setq web-mode-markup-indent-offset 2)
+            (setq web-mode-enable-auto-closing t)
+            (setq web-mode-enable-auto-pairing t)))
 
 (add-hook 'emmet-mode-hook
           (lambda()
